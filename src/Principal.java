2f9da6fc660f62f8b38e861c1d791043dcf3adde
admin <admin@OSMARVM0>
2017-11-08 16:29:42 -0200

    Comentários adicionais

--- src/Principal.java
+++ src/Principal.java
@@ -14,9 +14,17 @@ public class Principal {
   
     /**
      * Ordenação por Insercao.
+     * 
+     * Inicialmente, pega os dois primeiros elementos de um vetor, ou seja, posições 0 e 1
+     * 1. Atribui o elemento na posição 1 como chave
+     * Compara a chave com todos os elementos cujos respectivos índices sejam inferiores
+     *    - Até que encontre um menor valor menor que o da chave; OU
+     *    - O início do vetor seja atingido.
+     * 2. Repete o processo até o último elemento do vetor     
+     * 
      * Complexidade no pior caso Theta(n^2)
      * Complexidade no caso m~edio/esperado Theta(n log n)
-     * Slide 44
+     * Slide 11
      * @param A Vetor a ser ordenado
      * @param n Quantidade de elementos do vetor
      */
@@ -24,11 +32,17 @@ public class Principal {
         for (int j = 1; j <= n; j++) {                              //c1 * n
             int chave = A[j];                                       //c2 * n-1
             //Inserir A[j] dentro da sequencia ordenada A[1..j-1]
+            //Compara chave com elementos posicionados antes no vetor(j-1)
+            //Para se pelo menos uma das situações ocorrer:
+            //Alcançar o início do vetor(i>=0) ou a elemento anterior for maior que a chave
             int i = j - 1;                                          //c3 * n-1
             while ((i >= 0) && (A[i] > chave)) {                    //c4 * Somatorio(2 até n)tj
+                //Desloca os elementos para abrir espa;o
                 A[i + 1] = A[i];                                    //c5 * Somatorio(2 até n)tj-1
+                //Decrementa o contador até o inicio do vetor
                 i = i - 1;                                          //c6 * Somatorio(2 até n)tj-1
-            } // posicao de insercao
+            } 
+            // posicao de insercao
             A[i + 1] = chave;                                       //c7 * n - 1
         }
     }
