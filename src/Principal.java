ea43beda098fdcec5b6ccac63815a41a24c56762
admin <admin@OSMARVM0>
2017-11-16 23:11:56 -0200

    Correção dos índices do vetor e comentários

--- src/Principal.java
+++ src/Principal.java
@@ -4,8 +4,18 @@
  * Programa de Pós-Graduação em Ciências da Computação - PROPG
  * Disciplinas: Projeto e Análise de Algoritmos
  * Prof Alexandre Gonçalves da Silva 
- * Baseado nos slides da aula 18/08/2017 
- * Classificação por Inserção/InsertSort
+ *
+ * Baseado nos slides 27 da aula 04/08/2017 e slides 11 da aula 18/08/2017  
+ *
+ * Página 12 Cormen 3 ed
+ *
+ * HeapSort por Inserção/InsertSort
+ *
+ * Atenção:
+ * Vetor em java inicia em 0, os algoritmos consideram início em 1.
+ * A subtraçào de -1 ocorre somente no local de acesso ao vetor ou matriz 
+ * para manter a compatibilidade entre os algoritmos.
+ * 
  */
 
 /**
@@ -29,44 +39,45 @@ public class Principal {
      * @param A Vetor a ser ordenado
      * @param n Quantidade de elementos do vetor
      */
-    public static void ordenaPorInsercao(int A[], int n) {        
-        for (int j = 1; j <= n; j++) {                              //c1 * n
-            int chave = A[j];                                       //c2 * n-1
+    public static void ordenaPorInsercao(int A[], int p, int n) {        
+        for (int j = p; j <= n; j++) {                                   //c1 * n
+            int chave = A[j-1];                                         //c2 * n-1
             //Inserir A[j] dentro da sequencia ordenada A[1..j-1]
             //Compara chave com elementos posicionados antes no vetor(j-1)
             //Para se pelo menos uma das situações ocorrer:
             //Alcançar o início do vetor(i>=0) ou a elemento anterior for maior que a chave
-            int i = j - 1;                                          //c3 * n-1
-            while ((i >= 0) && (A[i] > chave)) {                    //c4 * Somatorio(2 até n)tj
+            int i = j - 1;                                              //c3 * n-1
+            while ((i >= p) && (A[i-1] > chave)) {                      //c4 * Somatorio(2 até n)tj
                 //Desloca os elementos para abrir espa;o
-                A[i + 1] = A[i];                                    //c5 * Somatorio(2 até n)tj-1
+                A[i + 1 - 1] = A[i-1];                                  //c5 * Somatorio(2 até n)tj-1
                 //Decrementa o contador até o inicio do vetor
-                i = i - 1;                                          //c6 * Somatorio(2 até n)tj-1
+                i = i - 1;                                              //c6 * Somatorio(2 até n)tj-1
             } 
             // posicao de insercao
-            A[i + 1] = chave;                                       //c7 * n - 1
+            A[i + 1 - 1] = chave;                                       //c7 * n - 1
         }
     }
 
     public static void main(String args[]) {
 
-        //Vetor dos dados    
+        //Vetor dos dados        
         int A[] = {50, 70, 60, 90, 10, 30, 20, 40};
      
         //Fim do vetor
-        int r = A.length - 1;
+        int r = A.length;
 
         System.out.println(">>> Inserção <<<");
         System.out.println("Original: ");
-        for (int i = 0; i <= r; i++) {
-            System.out.println((i) + " - " + A[i]);
+        for (int i = 0; i < r; i++) {
+            System.out.println((i+1) + " - " + A[i]);
         }
 
-        ordenaPorInsercao(A, r);
+        //ordenaPorInsercao(A, r);
+        ordenaPorInsercao(A, 1, r);
 
         System.out.println("Depois: ");
-        for (int i = 0; i <= r; i++) {
-            System.out.println((i) + " - " + A[i]);
+        for (int i = 0; i < r; i++) {
+            System.out.println((i+1) + " - " + A[i]);
         }
     }
 }
